5993b675a04c43156450eec1f430eb21ef10b4e1
traviscomber <n3uralia@gmail.com>
2026-07-24 09:03:42 -0400

    feat: register Philz model in N3uralia registry

--- n3uralia/models/registry.py
+++ n3uralia/models/registry.py
@@ -4,10 +4,13 @@ N3uralia Model Registry
 Central place to register available enhancement models.
 """
 
+from n3uralia.models.philz_model import PhilzClarityModel
+
 
 class ModelRegistry:
     def __init__(self):
         self.models = {}
+        self.register(PhilzClarityModel())
 
     def register(self, model):
         self.models[model.name] = model
